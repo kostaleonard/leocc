036d26c5f327df79ce3edee70e4bc3c03f15fc7a
leonard.kosta <kostaleonard@gmail.com>
2025-07-03 08:48:34 -0400

    Added file and line number of error
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- include/CExceptionConfig.h
+++ include/CExceptionConfig.h
@@ -5,7 +5,10 @@
 #ifndef INCLUDE_CEXCEPTIONCONFIG_H_
 #define INCLUDE_CEXCEPTIONCONFIG_H_
 #include <stdio.h>
+#include <stdlib.h>
 
-#define CEXCEPTION_NO_CATCH_HANDLER(id) printf("throw with no catch\n")
+#define CEXCEPTION_NO_CATCH_HANDLER(id) \
+    printf("Uncaught Throw! ID: %u at %s:%d\n", (id), __FILE__, __LINE__); \
+    exit((int)(id))
 
 #endif
