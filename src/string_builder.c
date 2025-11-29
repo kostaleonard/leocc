a1b24b8101b490b3beb9363a67a137689e2ac511
leonard.kosta <kostaleonard@gmail.com>
2025-11-29 07:44:50 -0500

    Added string_builder.c
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

---
+++ src/string_builder.c
@@ -0,0 +1,14 @@
+#include "include/string_builder.h"
+
+string_builder_t *string_builder_create() {
+    // TODO
+    return NULL;
+}
+
+void string_builder_append(char *s) {
+    // TODO
+}
+
+void string_builder_destroy(string_builder_t *sb) {
+    // TODO
+}
