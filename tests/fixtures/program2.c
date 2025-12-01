ce468867d24b9a57346d6c10e38b7f7762e82874
leonard.kosta <kostaleonard@gmail.com>
2025-12-01 07:23:56 -0500

    Added test program with comments, variables, and a binary op
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

---
+++ tests/fixtures/program2.c
@@ -0,0 +1,11 @@
+/**
+ * @brief This program introduces comments, variables, and a binary op.
+ */
+
+int main() {
+    // Here's where we're going to make a variable.
+    int x = 2025; // This is our variable.
+    //Dead code, please ignore: int y = 0;
+    /*Don't ignore this one.*/ int z = -8; 
+    return x + z;
+}
